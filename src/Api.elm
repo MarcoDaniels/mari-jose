bba5dca6c2a56ba66312c6a1655f32b4590134e0
Marco Martins <marcodanielmartins@gmail.com>
2021-08-06 22:34:00 +0200

    Rename

--- src/Api.elm
+++ src/Api.elm
@@ -2,13 +2,13 @@ module Api exposing (routes)
 
 import ApiRoute
 import DataSource exposing (DataSource)
-import Html exposing (Html)
+import Html as ElmHtml
 import Route exposing (Route)
 
 
 routes :
     DataSource (List Route)
-    -> (Html Never -> String)
+    -> (ElmHtml.Html Never -> String)
     -> List (ApiRoute.ApiRoute ApiRoute.Response)
 routes _ _ =
     []
